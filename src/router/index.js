ed64c58a807531b974437358900397155fa42f97
Yayak Yogi <yayaktaka@gmail.com>
2021-09-14 11:15:05 +0700

    add tab navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// impor halaman dari folder pages
-import {Splash, Login} from '../pages';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// halaman
+import {Splash, Login, MitraDashboard, MitraProfile} from '../pages';
+// component
+import {BottomTab} from '../components';
+
+// variabel untuk menentukan route
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator tabBar={props => <BottomTab {...props} />}>
+      <Tab.Screen
+        name="Dashboard"
+        component={MitraDashboard}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={MitraProfile}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Router = () => {
   return (
@@ -17,6 +41,11 @@ const Router = () => {
       <Stack.Screen
         name="Login"
         component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
