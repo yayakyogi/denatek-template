960813799474fbbabc75f7e8ae658bb6b0f1f696
Yayak Yogi <yayaktaka@gmail.com>
2021-09-14 11:04:32 +0700

    add icon

---
+++ src/assets/icon/index.js
@@ -0,0 +1,21 @@
+import ICDropdown from './ic_dropdown.svg';
+import ICHomeActive from './ic_home_aktif.svg';
+import ICHomeInActive from './ic_home_nonaktif.svg';
+import ICProfileActive from './ic_profile_aktif.svg';
+import ICProfileInActive from './ic_profile_nonaktif.svg';
+import ICSortDown from './ic_sort_down.svg';
+import ICButtonDel from './ic_btn_del_photo.svg';
+import ICButtonAdd from './ic_btn_add_photo.svg';
+import ICButtonCls from './ic_btn_close.svg';
+
+export {
+  ICDropdown,
+  ICHomeActive,
+  ICHomeInActive,
+  ICProfileActive,
+  ICProfileInActive,
+  ICSortDown,
+  ICButtonDel,
+  ICButtonAdd,
+  ICButtonCls,
+};
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,3 @@
 export * from './ilustration';
+export * from './icon';
+export * from './dummy';
