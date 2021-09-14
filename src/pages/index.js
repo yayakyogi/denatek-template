6af6786cce7d06c93efc6d58253a52f1cfe73364
Yayak Yogi <yayaktaka@gmail.com>
2021-09-14 11:02:02 +0700

    add pages dashboard and profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Splash from './Splash';
 import Login from './Login';
+import MitraDashboard from './MitraDashboard';
+import MitraProfile from './MitraProfile';
 
-export {Splash, Login};
+export {Splash, Login, MitraDashboard, MitraProfile};
