3e0a186865bcb9425ba8d2cbba4b4722c98f4c2a
Yayak Yogi <yayaktaka@gmail.com>
2021-09-14 21:38:12 +0700

    add ilustration

--- src/assets/ilustration/index.js
+++ src/assets/ilustration/index.js
@@ -1,3 +1,33 @@
-import ILApp from './ic_logo_denatek.png';
+import ILDeliveryMan from './delivery-man.png';
+import ILCheckIn from './checkin.png';
+import ILCheckOut from './checkout.png';
+import ILSetStatus from './set-status.png';
+import ILDetailKunjungan from './detail-kunjungan.png';
+import ILKirimLaporan from './kirim-laporan.png';
+import ILMitraBaru from './mitra-baru.png';
+import ILUserDefault from './user.png';
+import ILProdukTerjual from './produk-terjual.png';
+import ILProdukToko from './produk-toko.png';
+import ILSisaProduk from './sisa-produk.png';
+import ILGetGalery from './get-galeri.png';
+import ILGetSelfie from './get-selfie.png';
+import ILLogout from './logout.png';
+import ILApp from './logo-denatek.png';
 
-export {ILApp};
+export {
+  ILDeliveryMan,
+  ILCheckIn,
+  ILCheckOut,
+  ILSetStatus,
+  ILDetailKunjungan,
+  ILKirimLaporan,
+  ILMitraBaru,
+  ILUserDefault,
+  ILProdukTerjual,
+  ILProdukToko,
+  ILSisaProduk,
+  ILGetSelfie,
+  ILGetGalery,
+  ILLogout,
+  ILApp,
+};
