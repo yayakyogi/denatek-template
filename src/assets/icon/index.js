f24e4f4a2ef820cd8efa0cc5f07e0dc0f4ebe528
Yayak Yogi <yayaktaka@gmail.com>
2021-09-14 21:29:49 +0700

    update icon

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,21 +1,21 @@
 import ICDropdown from './ic_dropdown.svg';
-import ICHomeActive from './ic_home_aktif.svg';
-import ICHomeInActive from './ic_home_nonaktif.svg';
-import ICProfileActive from './ic_profile_aktif.svg';
-import ICProfileInActive from './ic_profile_nonaktif.svg';
 import ICSortDown from './ic_sort_down.svg';
 import ICButtonDel from './ic_btn_del_photo.svg';
 import ICButtonAdd from './ic_btn_add_photo.svg';
 import ICButtonCls from './ic_btn_close.svg';
+import ICHomeOn from './ic_home_active.png';
+import ICHomeOff from './ic_home_inactive.png';
+import ICProfileOn from './ic_profil_active.png';
+import ICProfileOff from './ic_profile_inactive.png';
 
 export {
   ICDropdown,
-  ICHomeActive,
-  ICHomeInActive,
-  ICProfileActive,
-  ICProfileInActive,
   ICSortDown,
   ICButtonDel,
   ICButtonAdd,
   ICButtonCls,
+  ICHomeOn,
+  ICHomeOff,
+  ICProfileOn,
+  ICProfileOff,
 };
--- src/pages/MitraDashboard/index.js
+++ src/pages/MitraDashboard/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, useColorScheme} from 'react-native';
+
+import {colors, fonts} from '../../utils';
+import {Gap} from '../../components';
 
 const MitraDashboard = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const darkMode = {
+    backgroundColor: isDarkMode
+      ? colors.darkMode.background
+      : colors.lightMode.background,
+  };
   return (
-    <View>
-      <Text>Dashboard</Text>
+    <View style={styles.container}>
+      <View style={[styles.wrapper, darkMode]}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainer={styles.scrollview}>
+          <Text>Dashboard</Text>
+        </ScrollView>
+      </View>
     </View>
   );
 };
 
 export default MitraDashboard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: colors.primary},
+  wrapper: {
+    flex: 1,
+    backgroundColor: colors.normal,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+});
