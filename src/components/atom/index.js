bbc45d7216063a44c91778a77328c8b76b3c25ee
Yayak Yogi <yayaktaka@gmail.com>
2021-09-14 11:12:53 +0700

    add atom component

--- src/components/atom/index.js
+++ src/components/atom/index.js
@@ -2,5 +2,6 @@ import Button from './Button';
 import TextInput from './TextInput';
 import Gap from './Gap';
 import ButtonText from './ButtonText';
+import ButtonSwitch from './ButtonSwitch';
 
-export {Button, TextInput, Gap, ButtonText};
+export {Button, TextInput, Gap, ButtonText, ButtonSwitch};
