337bc27e64d8f49bc4c000b726faa1282155fbe6
Yayak Yogi <yayaktaka@gmail.com>
2021-09-14 11:09:30 +0700

    add pages"

---
+++ src/pages/MitraDashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const MitraDashboard = () => {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
+
+export default MitraDashboard;
+
+const styles = StyleSheet.create({});
---
+++ src/pages/MitraProfile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const MitraProfile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default MitraProfile;
+
+const styles = StyleSheet.create({});
