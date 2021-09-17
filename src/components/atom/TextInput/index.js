e0780d8e742f83ffb7bbbddad34445b62833fd5c
Yayak Yogi <yayaktaka@gmail.com>
2021-09-17 22:57:36 +0700

    fix margin

--- src/components/atom/TextInput/index.js
+++ src/components/atom/TextInput/index.js
@@ -3,11 +3,11 @@ import {StyleSheet, Text, TextInput as InputText} from 'react-native';
 
 import {fonts} from '../../../utils';
 
-const TextInput = ({label, darkTheme, ...props}) => {
+const TextInput = ({label, darkMode, ...props}) => {
   return (
     <>
-      <Text style={[styles.label, darkTheme]}>{label}</Text>
-      <InputText {...props} style={[styles.textinput, darkTheme]} />
+      <Text style={[styles.label, darkMode]}>{label}</Text>
+      <InputText {...props} style={[styles.textinput, darkMode]} />
     </>
   );
 };
@@ -19,7 +19,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: fonts.primary.REG,
     marginBottom: 6,
-    marginTop: 24,
   },
   textinput: {borderWidth: 1, padding: 12, borderRadius: 10},
 });
