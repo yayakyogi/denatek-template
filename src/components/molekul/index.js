5e5183265b52106af01e546d32636e3d4e311976
Yayak Yogi <yayaktaka@gmail.com>
2021-09-17 22:58:28 +0700

    add component

--- src/components/molekul/index.js
+++ src/components/molekul/index.js
@@ -1,7 +1,8 @@
 import ButtonFlex from './ButtonFlex';
 import BottomTab from './BottomTab';
+import ButtonList from './ButtonList';
 import MenuFlex from './MenuFlex';
 import PhotoAbsent from './PhotoAbsent';
 import PhotoLaporan from './PhotoLaporan';
 
-export {ButtonFlex, BottomTab, MenuFlex, PhotoAbsent, PhotoLaporan};
+export {ButtonFlex, BottomTab, MenuFlex, PhotoAbsent, PhotoLaporan, ButtonList};
