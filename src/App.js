49007a1c4b7102d01d493a7256ada5b291cedd14
Yayak Yogi <yayaktaka@gmail.com>
2021-09-18 13:00:30 +0700

    set provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import Router from '../src/router';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
+const MainApp = () => {
+  <NavigationContainer>
+    <Router />
+  </NavigationContainer>;
+};
 const App = () => {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
