2b21c1312dd4ff641dd9a2efe6854a0701ca1317
Yayak Yogi <yayaktaka@gmail.com>
2021-09-20 07:12:16 +0700

    add component loading

--- src/components/molekul/index.js
+++ src/components/molekul/index.js
@@ -4,5 +4,14 @@ import ButtonList from './ButtonList';
 import MenuFlex from './MenuFlex';
 import PhotoAbsent from './PhotoAbsent';
 import PhotoForm from './PhotoForm';
+import Loading from './Loading';
 
-export {ButtonFlex, BottomTab, MenuFlex, PhotoAbsent, PhotoForm, ButtonList};
+export {
+  ButtonFlex,
+  BottomTab,
+  MenuFlex,
+  PhotoAbsent,
+  PhotoForm,
+  ButtonList,
+  Loading,
+};
