7eb91ba20c2db76b57afd90446488c0cb1486f36
Yayak Yogi <yayaktaka@gmail.com>
2021-09-20 07:21:40 +0700

    add action set default global state

--- src/redux/action/mitraAction.js
+++ src/redux/action/mitraAction.js
@@ -1,3 +1,5 @@
+import {API_HOST, queryString} from '../../utils';
+
 // set photoUri
 export const photoUriAbsent = (value, valueBase64) => {
   return {type: 'SET_PHOTO_ABSENT', value, valueBase64};
@@ -13,4 +15,52 @@ export const deletePhoto = () => {
 // set loading
 export const setLoading = value => {
   return {type: 'SET_LOADING', value};
+};
+// set default semua state
+export const setDefault = () => {
+  return {type: 'SET_DEFAULT'};
+};
+// send location
+export const sendLocation = (type, data, token, navigation) => dispatch => {
+  if (type === 'Canvaser') {
+    fetch(`${API_HOST.url}/canvaser/gps?${queryString(data)}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response.status === 401) {
+          console.log('Gagal mengirim lokasi');
+        } else {
+          console.log(response);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    fetch(`${API_HOST.url}/marketer/gps?${queryString(data)}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response.status === 401) {
+          console.log('Gagal mengirim lokasi');
+        } else {
+          console.log(response);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
