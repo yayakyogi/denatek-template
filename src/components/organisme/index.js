e55546625acfd6b3e9512ded60651d94ca284118
Yayak Yogi <yayaktaka@gmail.com>
2021-09-20 07:13:04 +0700

    remove some component

--- src/components/organisme/index.js
+++ src/components/organisme/index.js
@@ -1,6 +1,5 @@
 import MenuDashboard from './MenuDashboard';
 import MenuProfile from './MenuProfile';
 import MenuAbsent from './MenuAbsent';
-import MenuVisitDetail from './MenuVisitDetail';
 
-export {MenuDashboard, MenuProfile, MenuAbsent, MenuVisitDetail};
+export {MenuDashboard, MenuProfile, MenuAbsent};
